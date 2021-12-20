f28ad62b69b47c96fb0e3cc43c9e69ead11af560
dudoserg <frielender@gmail.com>
2021-12-21 02:16:17 +0700

    add exit mode

--- threads/Queue.h
+++ threads/Queue.h
@@ -37,6 +37,20 @@ public:
         std::unique_lock<std::mutex> lock(this->d_mutex);
         d_queue.clear();
     }
+
+
+    bool isCanStop() {
+        std::unique_lock<std::mutex> lock(this->d_mutex);
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+        this->d_condition.wait(
+            lock, [=] {
+                return (this->d_queue.empty());
+            }
+        );
+        return true;
+    }
+
     Functor pop() {
         std::unique_lock<std::mutex> lock(this->d_mutex);
         std::this_thread::sleep_for(std::chrono::milliseconds(1000));
